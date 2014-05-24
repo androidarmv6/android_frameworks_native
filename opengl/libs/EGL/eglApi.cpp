cd788395a70422bf6ebf0340f7a6048e507a7521
Conn O'Griofa <connogriofa@gmail.com>
2014-05-24 15:49:55 +0100

    Initial support for Broadcom VideoCore / BCM21553
    This commit adds Broadcom support for:
    * frameworks/native/opengl
    * services/surfaceflinger
    
    Enable via:
    * BOARD_USES_BROADCOM_HARDWARE := true
    * COMMON_GLOBAL_CFLAGS += BCM_HARDWARE
    
    Change-Id: Iba16d9c504a386f8a47c8b33962b7c079f380bd5

--- opengl/libs/EGL/eglApi.cpp
+++ opengl/libs/EGL/eglApi.cpp
@@ -1458,6 +1458,36 @@ EGLBoolean eglSignalSyncKHR(EGLDisplay dpy, EGLSyncKHR sync, EGLenum mode) {
     return result;
 }
 
+#ifdef BCM_HARDWARE
+EGLClientBuffer eglGetRenderBufferANDROID(EGLDisplay dpy, EGLSurface draw)
+{
+    SurfaceRef _s(draw);
+    if (!_s.get()) return setError(EGL_BAD_SURFACE, (EGLClientBuffer*)0);
+
+    egl_display_t const * const dp = validate_display(dpy);
+    egl_surface_t const * const s = get_surface(draw);
+    if (s->cnx->egl.eglGetRenderBufferANDROID) {
+        return s->cnx->egl.eglGetRenderBufferANDROID(
+                dp->disp[s->impl].dpy, s->surface);
+    }
+    return setError(EGL_BAD_DISPLAY, (EGLClientBuffer*)0);
+}
+
+void* eglGetComposerANDROID(EGLDisplay dpy, EGLSurface draw)
+{
+    SurfaceRef _s(draw);
+    if (!_s.get()) return setError(EGL_BAD_SURFACE, (EGLClientBuffer*)0);
+
+    egl_display_t const * const dp = validate_display(dpy);
+    egl_surface_t const * const s = get_surface(draw);
+    if (s->cnx->egl.eglGetComposerANDROID) {
+        return (void*)s->cnx->egl.eglGetComposerANDROID(
+                dp->disp[s->impl].dpy, s->surface);
+    }
+    return setError(EGL_BAD_DISPLAY, (EGLClientBuffer*)0);
+}
+#endif
+
 EGLint eglClientWaitSyncKHR(EGLDisplay dpy, EGLSyncKHR sync,
         EGLint flags, EGLTimeKHR timeout)
 {
