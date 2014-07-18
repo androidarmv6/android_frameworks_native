aab14dafed25c9a3491568240608d1329b1f3df1
AndroidMeda <hephappy@gmail.com>
2014-07-18 13:21:52 +0200

    Revert "Revert "free EGL resources associated to a thread when it terminates""
    This reverts commit 4a28fa52a0e2980e70bc40831a2470ce0d53ce87.
    
    Change-Id: Ib28cc12670aeb1d5dec967f4cc6737e0ee0bad60

--- opengl/libs/EGL/eglApi.cpp
+++ opengl/libs/EGL/eglApi.cpp
@@ -1296,6 +1296,11 @@ EGLBoolean eglReleaseThread(void)
 {
     clearError();
 
+#if EGL_TRACE
+    if (getEGLDebugLevel() > 0)
+        GLTrace_eglReleaseThread();
+#endif
+
     // If there is context bound to the thread, release it
     egl_display_t::loseCurrent(get_context(getContext()));
 
@@ -1303,12 +1308,7 @@ EGLBoolean eglReleaseThread(void)
     if (cnx->dso && cnx->egl.eglReleaseThread) {
         cnx->egl.eglReleaseThread();
     }
-
     egl_tls_t::clearTLS();
-#if EGL_TRACE
-    if (getEGLDebugLevel() > 0)
-        GLTrace_eglReleaseThread();
-#endif
     return EGL_TRUE;
 }
 
